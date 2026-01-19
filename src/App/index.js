a407399530ecf98c2ce352b0b0e18769b0e4594a
ManuelVParra <manXDuelyt@gmail.com>
2026-01-19 10:47:14 -0600

    Composición App Stateless
    Aquí se empieza a maquetar la composición de la app sin tener en cuenta el manejo del estado

--- src/App/index.js
+++ src/App/index.js
@@ -3,12 +3,54 @@ import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
 
+//Utilizando react context
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
+
+
+//Utilizando composición de componentes
 function App() {
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader/> 
+      <TodoList/>
+    </React.Fragment>
   );
+}
+
+
+function TodoHeader(){
+  return(
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+    </React.Fragment>
+  );
+}
+
+function TodoList(){
+  return (
+    <React.Fragment>
+      <TodoItem></TodoItem>
+    </React.Fragment>
+  );
+}
+
+function TodoCounter(){
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch(){
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem(){
+  return <p>TodoItem</p>;
 }
 
 export default App;
