0d8c7301040087712f0879fa564c6f8d87d8dbb2
ManuelVParra <manXDuelyt@gmail.com>
2026-01-19 12:31:41 -0600

    Composición saludable

--- src/App/index.js
+++ src/App/index.js
@@ -4,60 +4,59 @@ import { AppUI } from './AppUI';
 
 
 //Utilizando react context
-// function App() {
-//   return (
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//   );
-// }
+function App() {
+  return (
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+  );
+}
 
 
 //Utilizando composición de componentes
-function App() {
-  const [state, setState] = React.useState('Estado compartido');
+// function App() {
+//   const [state, setState] = React.useState('Estado compartido');
 
-  return (
-    <React.Fragment>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
+//   return (
+//     <React.Fragment>
+//       <TodoHeader>
+//         <TodoCounter />
+//         <TodoSearch />
+//       </TodoHeader>
 
-      <TodoList>
-        <TodoItem state={state}/>
-      </TodoList>
-    </React.Fragment>
-  );
-}
+//       <TodoList>
+//         <TodoItem state={state}/>
+//       </TodoList>
+//     </React.Fragment>
+//   );
+// }
 
+// function TodoHeader({children}){
+//   return(
+//     <header>
+//       {children}
+//     </header>
+//   );
+// }
 
-function TodoHeader({children}){
-  return(
-    <header>
-      {children}
-    </header>
-  );
-}
+// function TodoList({children}){
+//   return (
+//     <section>
+//       {children}
+//     </section>
+//   );
+// }
 
-function TodoList({children}){
-  return (
-    <section>
-      {children}
-    </section>
-  );
-}
+// function TodoCounter(){
+//   return <p>TodoCounter</p>;
+// }
 
-function TodoCounter(){
-  return <p>TodoCounter</p>;
-}
+// function TodoSearch(){
+//   return <p>TodoSearch</p>;
+// }
 
-function TodoSearch(){
-  return <p>TodoSearch</p>;
-}
-
-function TodoItem({state}){
-  return <p>TodoItem: {state}</p>;
-}
+// function TodoItem({state}){
+//   return <p>TodoItem: {state}</p>;
+// }
 
 export default App;
