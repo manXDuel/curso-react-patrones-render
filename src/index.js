b0d99b1d1739f3e59977dfda017550103ec7a693
ManuelVParra <manXDuelyt@gmail.com>
2026-01-21 11:52:20 -0600

    Ejercicio HOC 1
    Practica base para HOC tomando "withAlgo" la cual recibe un componente en este caso "App" para convertirlo en "WrappedComponent" y retornarlo con "ComponenteDeVerdad" con las props que se le esta pasando al momento de llamarlo desde "AppWithAlgo"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return(
+    <h1>{props.saludo}, {props.nombre}</h1>
+  );
+}
+
+function withAlgo(WrappedComponent){ //Función de orden superior
+  return function ComponenteDeVerdad(props){
+    //La función que retorna siempre debe ser un componente de React
+    return (
+      <React.Fragment>
+        <WrappedComponent {...props}/> 
+        <p>Acompañando al Wrapped Component</p>
+      </React.Fragment>
+    );
+  }
+}
+
+const AppWithAlgo = withAlgo(App);
+
+
 ReactDOM.render(
-  <App />,
+  <AppWithAlgo saludo="Hey" nombre="Juan"/>,
   document.getElementById('root')
 );
